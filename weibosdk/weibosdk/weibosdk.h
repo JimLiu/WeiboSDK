4ac88e647af260d31a36131481d27e217ef80846
JimLiu <junminliu@gmail.com>
2012-09-19 02:01:16 +0800

    封装为静态库，支持iOS6

---
+++ weibosdk/weibosdk/weibosdk.h
@@ -0,0 +1,13 @@
+//
+//  weibosdk.h
+//  weibosdk
+//
+//  Created by junmin liu on 12-9-12.
+//  Copyright (c) 2012年 idfsoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface weibosdk : NSObject
+
+@end
---
+++ weibosdk/weibosdk/weibosdk.m
@@ -0,0 +1,13 @@
+//
+//  weibosdk.m
+//  weibosdk
+//
+//  Created by junmin liu on 12-9-12.
+//  Copyright (c) 2012年 idfsoft. All rights reserved.
+//
+
+#import "weibosdk.h"
+
+@implementation weibosdk
+
+@end
